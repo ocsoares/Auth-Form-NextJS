3320911b42f8b44d2817421e34e053eb758265fa
ocsoares <casoares016@gmail.com>
2023-10-02 20:30:36 -0300

    feat: apply AppThemeProvider to Root Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import { PropsWithChildren } from "react";
 import { Metadata } from "next";
+import { CssBaseline } from "@mui/material";
+import { AppThemeProvider } from "@/shared/providers/AppThemeProvider";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export const metadata: Metadata = {
   title: "Auth Form",
@@ -9,7 +15,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <AppThemeProvider>
+        <CssBaseline />
+        <body>{children}</body>
+      </AppThemeProvider>
     </html>
   );
 }
