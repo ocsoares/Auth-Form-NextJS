7e19e4d1966cbbfa1f5d3a6ac502c1ad50741922
ocsoares <casoares016@gmail.com>
2023-10-02 20:25:56 -0300

    feat: add MaterialUI DarkTheme

---
+++ src/shared/themes/DarkTheme.ts
@@ -0,0 +1,30 @@
+import { createTheme } from "@mui/material";
+import { purple, teal, deepPurple, grey } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+  palette: {
+    mode: "dark",
+
+    primary: {
+      main: "#3D1870",
+      dark: "#250D46",
+      light: purple["500"],
+      contrastText: grey["500"],
+    },
+    secondary: {
+      main: grey["500"],
+      dark: teal["800"],
+      light: teal["500"],
+      contrastText: deepPurple["200"],
+    },
+    background: {
+      default: "#1F1032",
+      paper: "#2C203B",
+    },
+  },
+  typography: {
+    h3: {
+      color: "#3D2670",
+    },
+  },
+});
