c45c4e957c5fd3b7b7c9da0451818b2f5b05a044
ocsoares <casoares016@gmail.com>
2023-10-03 13:41:43 -0300

    feat: change "registration" route to "signup"

--- src/app/auth/signup/layout.tsx
+++ src/app/auth/signup/layout.tsx
@@ -2,10 +2,10 @@ import { Metadata } from "next";
 import { PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
-  title: "Registration",
-  description: "Registration form",
+  title: "Sign up",
+  description: "Sign up form",
 };
 
-export default function RegistrationLayout({ children }: PropsWithChildren) {
+export default function SignUpLayout({ children }: PropsWithChildren) {
   return <section>{children}</section>;
 }
