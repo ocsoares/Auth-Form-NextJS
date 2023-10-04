c98eb837ad48cb95373d572e7cbb565d4769b663
ocsoares <casoares016@gmail.com>
2023-10-04 19:28:32 -0300

    feat: add AuthFormFields in signup page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,10 +1,10 @@
-import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 import { SignUpBox } from "./components/SignUpBox";
 import { Lock } from "@mui/icons-material";
-import { AuthTextField } from "../components/AuthTextField";
 import { AuthAvatar } from "../components/AuthAvatar";
 import { AuthButton } from "../components/AuthButton";
 import { AuthTextLink } from "../components/AuthTextLink";
+import { AuthFormFields } from "./components/AuthFormFields";
 
 export default function SignUpForm() {
   return (
@@ -36,41 +36,7 @@ export default function SignUpForm() {
           <AuthAvatar avatar={<Lock color="secondary" />} />
           <Typography variant="h5">Sign up</Typography>
           <SignUpBox>
-            <Grid container spacing={4}>
-              <AuthTextField
-                autoFocus={true}
-                sm={6}
-                id="first-name"
-                name="first-name"
-                type="text"
-                label="First name"
-              />
-              <AuthTextField
-                sm={6}
-                id="last-name"
-                name="last-name"
-                type="text"
-                label="Last name"
-              />
-              <AuthTextField
-                id="email"
-                name="email"
-                type="email"
-                label="Email"
-              />
-              <AuthTextField
-                id="password"
-                name="password"
-                type="password"
-                label="Password"
-              />
-              <AuthTextField
-                id="confirm-password"
-                name="confirm-password"
-                type="password"
-                label="Confirm your password"
-              />
-            </Grid>
+            <AuthFormFields />
             <AuthButton text="Sign Up" />
             <AuthTextLink
               text="Already have an account? "
