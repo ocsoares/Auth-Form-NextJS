df26cb9f0069c5a97d850b52a1687a6bd3289794
ocsoares <casoares016@gmail.com>
2023-10-04 19:29:38 -0300

    chore: console.log in SignUpBox form data

--- src/app/auth/signup/components/SignUpBox.tsx
+++ src/app/auth/signup/components/SignUpBox.tsx
@@ -6,11 +6,15 @@ import { PropsWithChildren } from "react";
 export function SignUpBox({ children }: PropsWithChildren) {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     const data = new FormData(event.currentTarget);
 
     console.log({
+      firstName: data.get("first-name"),
+      lastName: data.get("last-name"),
       email: data.get("email"),
       password: data.get("password"),
+      confirmPassword: data.get("confirm-password"),
     });
   };
 
