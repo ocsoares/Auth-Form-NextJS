073f16555f3a06b27610be6d896e7bb56a777f2f
ocsoares <casoares016@gmail.com>
2023-10-04 13:23:55 -0300

    feat: add login page

---
+++ src/app/auth/login/components/LoginBox.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { PropsWithChildren } from "react";
+
+export function LoginBox({ children }: PropsWithChildren) {
+  // MUDAR ISSO para o Login !!!!
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <div>
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        {children}
+      </Box>
+    </div>
+  );
+}
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,97 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { LockPerson } from "@mui/icons-material";
+import { LoginBox } from "./components/LoginBox";
+import { AuthTextField } from "../components/AuthTextField";
+
 export default function LoginForm() {
   return (
-    <div>
-      <h2>Login Form</h2>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 5,
+          marginBottom: 5,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#8F3098" }}>
+            <LockPerson color="secondary" />
+          </Avatar>
+          <Typography variant="h5">Login</Typography>
+          <LoginBox>
+            <Grid container spacing={4}>
+              <AuthTextField
+                autoFocus={true}
+                id="email"
+                name="email"
+                type="email"
+                label="Email"
+              />
+              <AuthTextField
+                id="password"
+                name="password"
+                type="password"
+                label="Password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+                sx={{ mt: 1, ml: 2.6 }}
+              />
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Typography variant="body2" color={"whitesmoke"}>
+                  {`Don't have an account?`}{" "}
+                  <Link
+                    href="signup"
+                    variant="body2"
+                    underline="hover"
+                    color={"slateblue"}
+                  >
+                    Sign up
+                  </Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          </LoginBox>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
