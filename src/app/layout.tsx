3e42c95bd64a24656dfc6ce21971f808e38716ce
ocsoares <casoares016@gmail.com>
2023-10-11 21:49:47 -0300

    feat: add next auth provider to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { AuthProvider } from "@/shared/providers/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Auth Form",
@@ -15,26 +16,28 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-BR">
-      <AppThemeProvider>
-        <CssBaseline />
-        <body style={{ position: "relative" }}>
-          {children}
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            align="center"
-            style={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              right: 0,
-              marginBottom: "20px",
-            }}
-          >
-            Copyright © {new Date().getFullYear()} Cauã Soares.
-          </Typography>
-        </body>
-      </AppThemeProvider>
+      <body style={{ position: "relative" }}>
+        <AuthProvider>
+          <AppThemeProvider>
+            <CssBaseline />
+            {children}
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              align="center"
+              style={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                right: 0,
+                marginBottom: "20px",
+              }}
+            >
+              Copyright © {new Date().getFullYear()} Cauã Soares.
+            </Typography>
+          </AppThemeProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
