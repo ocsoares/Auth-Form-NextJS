229cef44ea8c5156e42873d5bd6013568915febb
ocsoares <casoares016@gmail.com>
2023-10-13 12:38:57 -0300

    feat: add PageWithAuth component to send email page

--- src/app/(routes)/send-email/page.tsx
+++ src/app/(routes)/send-email/page.tsx
@@ -1,3 +1,9 @@
+import { PageWithAuth } from "@/shared/components/PageWithAuth";
+
 export default function SendEmailPage() {
-  return <h2>Send Email</h2>;
+  return (
+    <PageWithAuth>
+      <h2>Send Email</h2>
+    </PageWithAuth>
+  );
 }
