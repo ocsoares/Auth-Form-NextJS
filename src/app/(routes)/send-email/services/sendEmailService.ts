3ef76a8a5f12b5cf6da17b05e0d43ad75ded34b8
ocsoares <casoares016@gmail.com>
2023-10-25 12:30:21 -0300

    feat: add bearer auth with JWT to send email service

--- src/app/(routes)/send-email/services/sendEmailService.ts
+++ src/app/(routes)/send-email/services/sendEmailService.ts
@@ -1,15 +1,19 @@
 import { ISendEmailData } from "../types/ISendEmailData";
 import { ISendEmailResponse } from "../types/ISendEmailResponse";
+import { getSession } from "next-auth/react";
 
 export const sendEmailService = async (
   data: ISendEmailData,
 ): Promise<ISendEmailResponse> => {
+  const session = await getSession();
+
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_SEND_EMAIL_SERVICE_URL}/send`,
       {
         method: "POST",
         headers: {
+          authorization: `bearer ${session?.user.jwt}`,
           "Content-type": "application/json",
         },
         body: JSON.stringify(data),
